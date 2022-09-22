4b9cb4d0c45f2aa8611b996c6ff5943c5884eb32
Gabriel-Shallon <shallonhatake@gmail.com>
2022-09-21 21:29:43 -0400

    Update Questao006.java

--- Questao006.java
+++ Questao006.java
@@ -14,26 +14,36 @@ public class Questao006 {
 
         int [] vetA = new int[num];
         int [] vetB = new int[num];
+        int [] vetC = new int[num];
 
     
         for (int i = 0; i < num; i++){
 
 
-            vetA[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o "+(i+1)+"° número."));
+            vetA[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o "+(i+1)+"° número (vetorA)."));
 
         }
 
         for (int i = 0; i < num; i++){
 
 
-            vetA[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o "+(i+1)+"° número."));
+            vetB[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o "+(i+1)+"° número (vetorB)."));
 
         }
 
 
+        for (int i = 0; i < num; i++){
+
+            vetC[i] = vetA[i]+vetB[i];
+
+        }
+
+        for (int i = 0; i < num; i++){
+
+            JOptionPane.showMessageDialog(null, "VetorA["+(i+1)+"] = "+vetA[i]+"\nVetorB["+(i+1)+"] = "+vetB[i]+"\nVetorC["+(i+1)+"] = "+vetC[i]+"\n\nVetorA["+(i+1)+"] + VetorB["+(i+1)+"] = VetorC["+(i+1)+"].");
 
 
-
+        }
 
 
             UIManager.put("OptionPane.yesButtonText","Sim");
